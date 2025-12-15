82f7b4822ea018639ee8b32464cb4e10938e6a0e
Yinon Bar <yinonbar1988@gmail.com>
2025-12-15 12:22:54 +0200

    config database connection variables seperate from code

--- 2-utils/Database.php
+++ 2-utils/Database.php
@@ -15,21 +15,27 @@ class Database
 
   public function connect()
   {
+    // שלב 1. יש להשיג את פרטי החיבור לדאטהבייס בגלל שאנחנו בפרודקשן
+    // אסור לשים פה את הפרטי חיבור ולכן יש להכניס אותם לקובץ נפרד
+    // בפרודקשן הקובץ נמצא במיקום מסויים ובסביבה הלוקאלית הוא נמצא במיקום אחר
     $localConfig = __DIR__ . '/config.php';                 // /public_html/2-utils/config.php
     $prodConfig  = dirname(__DIR__, 2) . '/config.php';     // /domains/.../config.php
     $configPath = file_exists($localConfig) ? $localConfig : $prodConfig;
 
+    // יש לוודא שאחד מהקבצים נמצא
     if (!file_exists($configPath)) {
       throw new RuntimeException("config.php not found at: $configPath");
     }
 
-    // טעינת פרטי החיבור מתוך קובץ config.php
+    // לאחר שבררנו את מיקום הקובץ עם פרטי החיבור
+    //יש לטעון את פרטי החיבור מתוך קובץ config.php
     $config = require $configPath;
     $host = $config['dbhost'];
     $db   = $config['dbname'];
     $user = $config['dbuser'];
     $pass = $config['dbpassword'];
 
+    // פונקציית PDO רגילה לחלוטין ליצירת חיבור לדאטהבייס
     try {
       $connection = new PDO('mysql:host=' . $host . ';dbname=' . $db, $user, $pass);
       $connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
